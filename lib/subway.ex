d64dc1083434ebeb0517d2a96334d38144505e15
AlexVKO <ale@alexvko.com>
2020-03-07 20:26:29 -0500

    Add basic functionality

--- lib/subway.ex
+++ lib/subway.ex
@@ -1,18 +1,50 @@
 defmodule Subway do
-  @moduledoc """
-  Documentation for `Subway`.
-  """
+  defmodule Broadcast do
+  end
 
-  @doc """
-  Hello world.
+  defmodule Event do
+  end
 
-  ## Examples
+  defmodule Import do
+    defmacro defevent(event_module, do_block) do
+      event_name = event_module |> Macro.to_string() |> Macro.underscore()
 
-      iex> Subway.hello()
-      :world
+      quote do
+        defmodule unquote(event_module) do
+          use Ecto.Schema
+          import Ecto.Changeset
 
-  """
-  def hello do
-    :world
+          schema unquote(event_name) <> "_events" do
+            unquote(do_block)
+
+            timestamps()
+          end
+
+          def broadcast(params) do
+            changeset =
+              %unquote(event_module){}
+              |> cast(params, unquote(event_module).__schema__(:fields))
+              |> validate_required([])
+
+            IO.puts("TODO: Broadcast: #{unquote(event_module)}")
+
+            case changeset.valid? do
+              true -> {:ok, changeset.changes}
+              false -> {:error, changeset.errors}
+            end
+          end
+        end
+
+        def broadcast(unquote(event_name), params) do
+          unquote(event_module).broadcast(params)
+        end
+      end
+    end
+  end
+
+  defmacro __using__(_) do
+    quote do
+      import Import
+    end
   end
 end
--- test/subway_test.exs
+++ test/subway_test.exs
@@ -1,8 +1,21 @@
+defmodule UserEvents do
+  use Subway
+
+  defevent ListingFavorited do
+    field(:listing_id, :string)
+  end
+end
+
+defmodule GenSubscriber do
+end
+
 defmodule SubwayTest do
   use ExUnit.Case
-  doctest Subway
 
-  test "greets the world" do
-    assert Subway.hello() == :world
+  test "it creates an changeset with the proper " do
+    case UserEvents.broadcast("listing_favorited", %{listing_id: "1"}) do
+      {:ok, payload} -> IO.inspect(payload)
+      {:error, errors} -> IO.inspect(errors)
+    end
   end
 end
